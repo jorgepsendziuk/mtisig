2633b28b071f5e2c3131760c380c07a379cdeda6
jimxxx666 <jimxxx@gmail.com>
2014-10-13 15:00:43 -0400

    Initial commit

---
+++ geopaparazzimarkerslib/build/generated/source/buildConfig/release/org/dsandler/apps/markers/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.dsandler.apps.markers;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "org.dsandler.apps.markers";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 40;
+  public static final String VERSION_NAME = "";
+}
